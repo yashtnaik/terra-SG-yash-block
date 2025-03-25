8c0ffc546a4f5889477322e53d7dc6fbee51032e
Yash Naik <yashtnaik@gmail.com>
2025-03-25 20:41:13 +0530

    gg

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = aws_vpc.My-Test-VPC-Terraform.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -27,7 +27,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  cidr_ipv6         = aws_vpc.My-Test-VPC-Terraform.ipv6_cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
